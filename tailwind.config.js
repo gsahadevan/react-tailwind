f199afec3aacac03cedd382897a2adda79026282
Gauthaman Sahadevan <gsahadevan@outlook.com>
2021-10-24 01:35:14 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	mode: 'jit',
+	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+	darkMode: 'class', // or 'media' or 'class' or false
+	theme: {
+		extend: {}
+	},
+	variants: {
+		extend: {}
+	},
+	plugins: []
+};
