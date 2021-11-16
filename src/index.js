560f658e19e9e16c3935fdd9c5f53bdc92f0f441
gsahadevan <gsahadevan@outlook.com>
2021-11-16 17:25:56 +0530

    added base url

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
     <React.StrictMode>
         <ThemeProvider>
             <SidebarProvider>
-                <BrowserRouter>
+                <BrowserRouter basename={process.env.PUBLIC_URL}>
                     <App />
                 </BrowserRouter>
             </SidebarProvider>
